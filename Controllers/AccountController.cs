e7bf4543b855df405408e26100e142e90c86214c
TnKhang14 <ktranf123@gmail.com>
2026-05-04 07:23:49 +0700

    test Login/Register

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TuNhanTamTInh_Ecommerce.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
